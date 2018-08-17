de89c23d08ae5c432ea2f060580c7efdeaeb0bbf
d074177 <d074177@c02ww0bzhtd8.dhcp.muc2.sap.corp>
2018-08-17 15:57:15 +0200

    initial publishing...

---
+++ src/main/java/demo/spring/cloud/RyanBaxterBeginnersGuideSpringCloudApplication.java
@@ -0,0 +1,12 @@
+package demo.spring.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RyanBaxterBeginnersGuideSpringCloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RyanBaxterBeginnersGuideSpringCloudApplication.class, args);
+	}
+}
